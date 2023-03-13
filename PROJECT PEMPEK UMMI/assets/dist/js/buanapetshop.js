3447ce01d95156c97c4b4ec9af49ff1d6e58c374
MuhHelmiKurniawan <81275634+MuhHelmiKurniawan@users.noreply.github.com>
2023-03-13 14:38:32 +0700

    Add files via upload

---
+++ PROJECT PEMPEK UMMI/assets/dist/js/buanapetshop.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$(".slider-utama").owlCarousel({
+		items:1,
+		loop:true,
+	});
+	$(".slider-produk").owlCarousel({
+		items:4,
+		margin:10,
+		nav:true,
+		navContainer:"#letaknavproduk",
+		navText:["<a class='btn btn-primary btn-xs'><i class='fa fa-chevron-left'></i></a>","<a class='btn btn-primary btn-xs'><i class='fa fa-chevron-right'></i></a>"],
+		dots:false,
+		responsive:{
+			0:{
+				items:2,
+			},
+			600:{
+				items:3,
+			},
+			1000:{
+				items:4,
+			}
+		}		
+	});
+	$(".slider-blog").owlCarousel({
+		items:2,
+		margin:10,
+		nav:true,
+		navContainer:"#letaknavblog",
+		navText:["<a class='btn btn-primary btn-xs'><i class='fa fa-chevron-left'></i></a>","<a class='btn btn-primary btn-xs'><i class='fa fa-chevron-right'></i></a>"],
+		dots:false,		
+	});
+	$(".slider-terlaris").owlCarousel({
+		items:1,
+		nav:true,
+		navContainer:"#letaknavterlaris",
+		navText:["<a class='btn btn-primary btn-xs'><i class='fa fa-chevron-left'></i></a>","<a class='btn btn-primary btn-xs'><i class='fa fa-chevron-right'></i></a>"],
+		dots:false,		
+	});
+	$(".slider-testimoni").owlCarousel({
+		items:1,	
+	});
+})
